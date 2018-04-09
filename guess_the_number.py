12933d6d01726b30eb53377de374549409c9a358
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-08 21:31:59 -0400

    Added better variable names.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,33 +1,33 @@
 import random
 import math
 
-x = math.floor(random.random() * 10 + 1)
-z = math.floor(random.random() * 20 + 1)
-counter = 0
+EASY = math.floor(random.random() * 10 + 1)
+HARD = math.floor(random.random() * 20 + 1)
+COUNTER = 0
 
-beginning = input("Please select a mode, (e)asy or (h)ard\n")
+BEGINNING = input("Please select a mode, (e)asy or (h)ard\n")
 
-if beginning == "e":
+if BEGINNING == "e":
     while True:
-        y = int(input("Guess a number between 1 and 10: "))
-        if x == y:
-            print("Good job, the number was {}".format(x))
+        GUESS = int(input("Guess a number between 1 and 10: "))
+        if GUESS == EASY:
+            print("Good job, the number was {}".format(EASY))
             break
         else:
             print("Please try again.")
-            counter += 1
-            if counter == 5:
+            COUNTER += 1
+            if COUNTER == 5:
                 print("Game over.")
                 break
-elif beginning == "h":
+elif BEGINNING == "h":
     while True:
-        y = int(input("Guess a number between 1 and 20: "))
-        if z == y:
-            print("Good job, the number was {}".format(z))
+        GUESS = int(input("Guess a number between 1 and 20: "))
+        if GUESS == HARD:
+            print("Good job, the number was {}".format(HARD))
             break
         else:
             print("Please try again.")
-            counter += 1
-            if counter == 3:
+            COUNTER += 1
+            if COUNTER == 3:
                 print("Game over.")
                 break
