f9df6f640806b88c5999de8fcd87dbd6a430aac8
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-08 21:49:20 -0400

    Added more comments.

--- guess_the_number.py
+++ guess_the_number.py
@@ -9,20 +9,26 @@ COUNTER = 0
 
 BEGINNING = input("Please select a mode, (e)asy or (h)ard\n")
 
+# This is easy mode. If the user picks e, then this is run. 
 if BEGINNING == "e":
     while True:
         GUESS = int(input("Guess a number between 1 and 10: "))
+        # Winning conditions.
         if GUESS == EASY:
             print("Good job, the number was {}".format(EASY))
             break
         else:
+            # If the user guesses wrong.
             print("Please try again.")
             COUNTER += 1
             # For testing purposes only.
             print("The guess is {0} and the counter is at {1}".format(EASY, COUNTER))
+            # Game over and loose conditions.
             if COUNTER == 5:
                 print("Game over.")
                 break
+# Same logic as above. When I learn more about classes and instances.
+# I will turn this into a Game class and make an easy and hard instance. 
 elif BEGINNING == "h":
     while True:
         GUESS = int(input("Guess a number between 1 and 20: "))
