7720f00fb9e54d022ab317f99338a4c1a754f9bb
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-09 01:47:54 -0400

    Added some extra instructions to both modes.

--- guess_the_number.py
+++ guess_the_number.py
@@ -12,6 +12,7 @@ BEGINNING = input("Please select a mode, (e)asy or (h)ard\n")
 # This is easy mode. If the user picks e, then this is run. 
 if BEGINNING == "e":
     while True:
+        print("You have 5 guesses to guess the right number. ")
         GUESS = int(input("Guess a number between 1 and 10: "))
         # Winning conditions.
         if GUESS == EASY:
@@ -31,6 +32,7 @@ if BEGINNING == "e":
 # I will turn this into a Game class and make an easy and hard instance. 
 elif BEGINNING == "h":
     while True:
+        print("You have 3 guesses to guess the number. ")
         GUESS = int(input("Guess a number between 1 and 20: "))
         if GUESS == HARD:
             print("Good job, the number was {}".format(HARD))
